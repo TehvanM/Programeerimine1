c6d06c7b706fead7a7c8e546ada2898918db7921
TehvanM <144792151+TehvanM@users.noreply.github.com>
2024-06-20 12:43:31 +0300

    1

---
+++ KT10.py
@@ -0,0 +1,16 @@
+import turtle
+
+aken = turtle.Screen()
+aken.setup(300,300)
+
+t = turtle
+
+for y in range(2):
+    for x in range(4):
+        t.forward(20)
+        t.right(90)
+        t.forward(100)
+        t.right(90)
+        t.forward(20)
+        t.right(90)
+        t.forward(80)
